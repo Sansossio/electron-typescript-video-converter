1653861ce340eb3f618ae0c1ddaec934c3f037e4
Sansossio <juliosansossio@gmail.com>
2020-04-20 19:31:35 +0200

    init ipc events

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import { BrowserWindow, app } from 'electron'
 import * as dotenv from 'dotenv'
 import { baseUrl } from './url/base-url'
+import { initIpc } from './ipc'
 
 dotenv.config()
 
 app.on('ready', () => {
   const main = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      webSecurity: false
     }
   })
   main.loadURL(baseUrl())
   main.webContents.openDevTools()
 })
+
+initIpc()
